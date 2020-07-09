4e482ef9cc90f6d3ed6134321210dd116dfd8678
knlih <psa-98@mail.ru>
2020-07-09 12:21:13 +0800

    T

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.View;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PFrame.Content = new TypePage();
+
+
+        }
+
+        private void PFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
---
+++ WpfApp1/ViewModel/TypeAchievementsVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+namespace WpfApp1.ViewModel
+{
+    class TypeAchievementsVM
+    {
+        private AttainmentEntities entities = null;
+
+        private ObservableCollection<TypeAchievementTBL> typeAchievement = null;
+        public ObservableCollection<TypeAchievementTBL> TypeAchievement
+        {
+            get { return typeAchievement; }
+            set
+            {
+                if(value != null)
+                  typeAchievement =  value;
+            }
+
+        }
+
+
+        public TypeAchievementsVM()
+        {
+            entities = new AttainmentEntities();
+            entities.TypeAchievementTBLs.Load();
+            var collect = entities.TypeAchievementTBLs.Local.ToBindingList();
+            TypeAchievement = new ObservableCollection<TypeAchievementTBL>(collect);
+        }
+    }
+}
